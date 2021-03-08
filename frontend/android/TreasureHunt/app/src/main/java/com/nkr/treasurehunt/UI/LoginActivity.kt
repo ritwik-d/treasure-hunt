4625ecaaeca23339755a43220bd699aa788f7a4e
I-am-the-mastermind <72708743+I-am-the-mastermind@users.noreply.github.com>
2021-03-07 20:25:52 -0800

    Updated frontend

---
+++ frontend/android/TreasureHunt/app/src/main/java/com/nkr/treasurehunt/UI/LoginActivity.kt
@@ -0,0 +1,70 @@
+package com.nkr.treasurehunt.UI
+
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.nkr.treasurehunt.R
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val login: Button? = findViewById(R.id.Login)
+
+        val emailOBJ = findViewById<EditText>(R.id.Email)
+        val pwdOBJ = findViewById<EditText>(R.id.Password)
+
+        login?.setOnClickListener({ view ->
+            //generateJsonAccountFile(emailOBJ.text.toString(), pwdOBJ.text.toString())
+        })
+    }
+
+    public fun generateJsonAccountFile(email: String?, pwd: String?) {
+        try {
+            //create a reference to the account directory
+            val root = File(Environment.getExternalStorageDirectory(), "account")
+
+            //check if the account directory exists
+            if (!root.exists()) {
+                //create the account directory
+                root.mkdirs()
+            }
+
+            //create a reference to the json account file
+            val gpxfile = File(root, "account.json")
+
+            //delete the file to override the file later
+            gpxfile.delete()
+
+            //create the json account file
+            gpxfile.createNewFile()
+
+            //create a file writer to write to the json file
+            val writer = FileWriter(gpxfile)
+
+            //create a map for the data
+            val map = mapOf("email" to email,"pwd" to pwd)
+
+            //convert the map to json format
+            val accountInfo = Gson().toJson(map)
+
+            //write json to the file
+            writer.append(accountInfo)
+            writer.flush()
+
+            //close the file
+            writer.close()
+        } catch (e: IOException) {
+            //a common error has occurred
+            e.printStackTrace()
+        }
+    }
+}
---
+++ frontend/android/TreasureHunt/app/src/main/java/com/nkr/treasurehunt/UI/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nkr.treasurehunt.UI
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.nkr.treasurehunt.R
+import java.io.File
+import java.io.FileReader
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //check if there is already a user logged in
+        if (checkForAccount()) {
+            //redirect the user to home screen
+            startActivity(Intent(this, HomeActivity::class.java).apply {
+                putExtra("email", readAccount())
+            })
+        }
+    }
+
+    private fun checkForAccount() : Boolean {
+        //get a reference to the folder
+        val root = File(Environment.getExternalStorageDirectory(), "account")
+
+        //return if it exists or not
+        return root.exists()
+    }
+
+    private fun readAccount() : HashMap<String, String> {
+        //read the json data from the local file
+        val accountData = Gson().fromJson(FileReader(File(File(Environment
+            .getExternalStorageDirectory(),
+            "account"), "account.json"))
+            .readText(), HashMap::class.java)
+                as HashMap<String, String>
+
+        //return the account info
+        return accountData
+    }
+}
