bb9ae94338099b16c585d141d397a5555603ac75
Nikhil-Dharanipragada <72708743+Nikhil-Dharanipragada@users.noreply.github.com>
2021-03-07 17:38:35 -0800

    laying out

--- backend/python/utils.py
+++ backend/python/utils.py
@@ -1,2 +1,33 @@
-def send_email(file_name: str, is_html=False, sender_email=None, receiver_email: str):
-    pass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from environment import *
+import mimetypes
+import smtplib
+
+config = Config(file_config(''))
+
+def send_email(file_name: str, receiver_email, sender_email=None, subject: str):
+    smtp_config = config.get('smtp')
+    final_path = f'''{config.get('file_paths', 'emails')}{file_name}'''
+    file_type = mimetypes.guess_type(final_path)
+    with open(final_path, 'r') as f:
+        message = f.read()
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    if file_type == 'text/html':
+        msg.attach(MIMEText(message, 'html'))
+    else:
+        msg.attach(MIMEText(message, 'plain'))
+
+    try:
+        with smtplib.SMTP(smtp_server, port) as server
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, msg.as_string())
+            return True
+    except smtplib.SMTPRecipientsRefused:
+        return False
