9f4b058d73fe08ba993a92c280d7ff28af6da3d4
I-am-the-mastermind <72708743+I-am-the-mastermind@users.noreply.github.com>
2021-03-09 09:18:57 -0800

    s

--- frontend/android/TreasureHunt/app/src/main/java/com/nkr/treasurehunt/UI/SignupActivity.kt
+++ frontend/android/TreasureHunt/app/src/main/java/com/nkr/treasurehunt/UI/SignupActivity.kt
@@ -1,20 +1,33 @@
 package com.nkr.treasurehunt.UI
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.nkr.treasurehunt.R
-import com.nkr.treasurehunt.UI.LoginActivity.Companion.generateJsonAccountFile
+import com.parse.ParseUser
+
 
 class SignupActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
         val button = findViewById<Button>(R.id.Signup)
+        val usernameOBJ = findViewById<EditText>(R.id.Username)
 
-        button.setOnClickListener{view ->
-            generateJsonAccountFile(null, null)
+        val myIcon = resources.getDrawable(R.drawable.ic_baseline_done_24, resources.newTheme())
+        myIcon.setBounds(0, 0, myIcon.intrinsicWidth, myIcon.intrinsicHeight)
+
+        usernameOBJ.setError("Username is available", myIcon)
+
+        button.setOnClickListener{ view ->
+            val user = ParseUser()
         }
     }
 }
