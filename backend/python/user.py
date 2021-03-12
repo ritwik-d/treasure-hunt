62d7e93cffec627d61f9b22933e0072ebd29e934
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-11 22:33:21 -0800

    Update user.py

--- backend/python/user.py
+++ backend/python/user.py
@@ -9,8 +9,11 @@ def authenticate(func):
     def wrapper(user, *args, **kwargs):
         db = DB()
         db.connect()
+        print(f'uid: {user.user_id}\npw: {user.pw}')
         if db.select('select * from users where user_id = %s and password = %s', params=(user.user_id, user.pw), dict_cursor=True) != tuple():
+            print('it worked')
             return func(user, *args, **kwargs)
+        print('it didnt work')
         return fail
     return wrapper
 
