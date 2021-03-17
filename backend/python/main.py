de4a4bc808650947acb228fc3701740683b47391
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-17 14:32:18 -0700

    stuff

--- backend/python/main.py
+++ backend/python/main.py
@@ -8,10 +8,10 @@ from user import *
 app = FastAPI()
 paths = config.get('api', 'paths')
 
-@app.post(paths.get('create_challenge'))
-async def create_challenge(json: CreateChallenge):
-    user = User(email=json.email, pw=json.pw, user_id=json.user_id)
-    return user.create_challenge(json.difficulty, json.latitude, json.longitude, json.name, json.puzzle, group_name=json.group_name)
+# @app.post(paths.get('create_challenge'))
+# async def create_challenge(json: CreateChallenge):
+#     user = User(email=json.email, pw=json.pw, user_id=json.user_id)
+#     return user.create_challenge(json.difficulty, json.latitude, json.longitude, json.name, json.puzzle, group_name=json.group_name)
 
 
 @app.post(paths.get('create_group'))
