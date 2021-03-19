26f371396940a8b4f15eec98c23dcb3c575b4841
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-19 10:52:11 -0700

    fixed bug

--- backend/python/db.py
+++ backend/python/db.py
@@ -82,7 +82,7 @@ class DB:
         sql = f'DELETE FROM {table_name} WHERE'
         for col in where_params:
             sql += f' {col} = %s,'
-        sql -= ','
+        sql = sql[0:len(sql) - 1]
         print(f'delete sql: {sql}')
         params = tuple(where_params.values())
         cursor.execute(sql, params)
