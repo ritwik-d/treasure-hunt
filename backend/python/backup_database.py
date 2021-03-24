9c9f05fc4618732e2dbe991ea224dfe34eba64dd
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-24 10:17:13 -0700

    Update backup_database.py

--- backend/python/backup_database.py
+++ backend/python/backup_database.py
@@ -23,15 +23,17 @@ def gzip_file(file_path: str):
 def main():
     dump_file_name = config.get('paths', 'tmp') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql'
     os.system(f'''mysqldump -u root -p{config.get('mysql', 'root_pw')} --databases {config.get('mysql', 'name')} > {dump_file_name}''')
-    dump_file_name = gzip_file(dump_file_name)
+    dump_file_name_2 = gzip_file(dump_file_name)
 
     fb_config = get_config()
     firebase = pyrebase.initialize_app(fb_config)
     storage = firebase.storage()
 
     cloud_path = config.get('firebase_storage', 'db_backups') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql.gz'
-    storage.child(cloud_path).put(dump_file_name)
-    
+    storage.child(cloud_path).put(dump_file_name_2)
+
+    os.remove(dump_file_name)
+    os.remove(dump_file_name_2)
 
 if __name__ == '__main__':
     main()
