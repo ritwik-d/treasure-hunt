1373d476fdc767f62832eeb562f4ba9d2d3ff9e1
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-23 17:22:42 -0700

    Update backup_database.py

--- backend/python/backup_database.py
+++ backend/python/backup_database.py
@@ -14,14 +14,14 @@ def get_config():
 
 
 def main():
-    dump_file_name = config.get('paths', 'tmp') + datetime.datetime.now().strftime('%m/%d/%Y') + '.sql'
+    dump_file_name = config.get('paths', 'tmp') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql'
     os.system(f"""root_pw='{config.get('mysql', 'root_pw')}'""")
     os.system(f'''mysqldump -u root -p$root_pw --databases {config.get('mysql', 'name')} > {dump_file_name}''')
     fb_config = get_config()
     firebase = pyrebase.initialize_app(fb_config)
     storage = firebase.storage()
 
-    cloud_path = config.get('firebase_storage', 'db_backups') + datetime.datetime.now().strftime('%m/%d/%Y') + '.sql'
+    cloud_path = config.get('firebase_storage', 'db_backups') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql'
     storage.child(cloud_path).put(dump_file_name)
 
     os.remove(dump_file_name)
