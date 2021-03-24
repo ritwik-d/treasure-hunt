99f18d87f647037cd5e8a82d75f535020a6d0f57
ritwikd <ritwik.deshpande.8@gmail.com>
2021-03-24 10:13:47 -0700

    Update backup_database.py

--- backend/python/backup_database.py
+++ backend/python/backup_database.py
@@ -17,22 +17,21 @@ def get_config():
 def gzip_file(file_path: str):
     with open(file_path, 'rb') as f_in, gzip.open(file_path + '.gz', 'wb') as f_out:
         f_out.writelines(f_in)
+        return file_path + '.gz'
 
 
 def main():
     dump_file_name = config.get('paths', 'tmp') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql'
     os.system(f'''mysqldump -u root -p{config.get('mysql', 'root_pw')} --databases {config.get('mysql', 'name')} > {dump_file_name}''')
-    gzip_file(dump_file_name)
+    dump_file_name = gzip_file(dump_file_name)
 
     fb_config = get_config()
     firebase = pyrebase.initialize_app(fb_config)
     storage = firebase.storage()
 
-    cloud_path = config.get('firebase_storage', 'db_backups') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql'
+    cloud_path = config.get('firebase_storage', 'db_backups') + datetime.datetime.now().strftime('%m-%d-%Y') + '.sql.gz'
     storage.child(cloud_path).put(dump_file_name)
-
-    os.remove(dump_file_name)
-
+    
 
 if __name__ == '__main__':
     main()
