cbc52b63478f55d3f3772d4e5d080389620a0f61
I-am-the-mastermind <72708743+I-am-the-mastermind@users.noreply.github.com>
2021-04-16 21:22:35 -0700

    in progress race frontend

--- frontend/android/TreasureHunt/app/src/main/java/com/ritwikscompany/treasurehunt/ui/RacesActivity.kt
+++ frontend/android/TreasureHunt/app/src/main/java/com/ritwikscompany/treasurehunt/ui/RacesActivity.kt
@@ -1,16 +1,70 @@
 package com.ritwikscompany.treasurehunt.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.TimePickerDialog
+import android.app.TimePickerDialog.OnTimeSetListener
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.ritwikscompany.treasurehunt.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class RacesActivity : AppCompatActivity() {
 
     private var userData = HashMap<String, Any>()
+    private val ctx = this@RacesActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_races)
 
         this.userData = intent.getSerializableExtra("userData") as HashMap<String, Any>
+
+        val scheduleRaceBTN = findViewById<Button>(R.id.races_schedule_race)
+        scheduleRaceBTN
+            .setOnClickListener {
+                scheduleRace()
+            }
+    }
+
+    private fun scheduleRace() {
+        
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun showDateDialog() {
+        val calendar: Calendar = Calendar.getInstance()
+        val dateSetListener =
+            OnDateSetListener { _, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val timeSetListener =
+                    OnTimeSetListener { _, hourOfDay, minute ->
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                        calendar.set(Calendar.MINUTE, minute)
+                        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
+                        n.setText(simpleDateFormat.format(calendar.time))
+                    }
+                TimePickerDialog(
+                    ctx,
+                    timeSetListener,
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE),
+                    false
+                ).show()
+            }
+
+        DatePickerDialog(
+            ctx,
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
     }
 }
