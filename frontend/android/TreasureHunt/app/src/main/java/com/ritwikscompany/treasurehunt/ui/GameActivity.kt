78b7a401b5b55998b82b72470d8ddd43e99409f4
ritwikd <ritwik.deshpande.8@gmail.com>
2021-04-17 19:00:40 -0700

    DISTANCE BETWEEN WORKS

--- frontend/android/TreasureHunt/app/src/main/java/com/ritwikscompany/treasurehunt/ui/GameActivity.kt
+++ frontend/android/TreasureHunt/app/src/main/java/com/ritwikscompany/treasurehunt/ui/GameActivity.kt
@@ -1,6 +1,5 @@
 package com.ritwikscompany.treasurehunt.ui
 
-import com.ritwikscompany.treasurehunt.R
 import android.Manifest
 import android.app.AlertDialog
 import android.content.Intent
@@ -9,6 +8,7 @@ import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -24,6 +24,7 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.ritwikscompany.treasurehunt.R
 import com.ritwikscompany.treasurehunt.utils.Utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -32,7 +33,6 @@ import kotlinx.coroutines.withContext
 import java.util.*
 import kotlin.collections.HashMap
 import kotlin.math.cos
-import kotlin.math.sin
 
 
 class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
@@ -62,9 +62,9 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         startLocationUpdates()
 
         val bodyJson = Gson().toJson(hashMapOf(
-            "user_id" to userData.get("user_id"),
-            "pw" to userData.get("password"),
-            "name" to challengeName
+                "user_id" to userData.get("user_id"),
+                "pw" to userData.get("password"),
+                "name" to challengeName
         ))
         CoroutineScope(Dispatchers.IO).launch {
             val (_, response, result) = Fuel.post("${getString(R.string.host)}/api/get_challenge_data")
@@ -92,7 +92,7 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                             findViewById<TextView>(R.id.game_puzzle).text = "Puzzle: ${challengeData["puzzle"] as String}"
                             findViewById<TextView>(R.id.game_creator).text = "Creator: ${challengeData["creator_name"] as String}"
 
-                            launchCompleteDaemon(challengeData)
+                            launchDaemon(challengeData)
                         }
                         else {
                             Toast.makeText(ctx, "Network Error", Toast.LENGTH_LONG).show()
@@ -203,7 +203,7 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             return
         }
 
-        fusedLocationClient.requestLocationUpdates(locationRequest, object: LocationCallback() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
                 lastLocation = p0.lastLocation
             }
@@ -211,25 +211,37 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     }
 
 
-    private fun launchCompleteDaemon(challengeData: HashMap<String, Any>) {
+    private fun launchDaemon(challengeData: HashMap<String, Any>) {
         CoroutineScope(Dispatchers.IO).launch {
+            var distanceTraveled = 0.0
+            var previousLocation = lastLocation
             while (true) {
                 Thread.sleep(1000)
-                withContext(Dispatchers.Main) {
-                    runOnUiThread {
-                        val results = FloatArray(1)
-                        Location.distanceBetween(
-                            lastLocation.latitude,
-                            lastLocation.longitude,
-                            challengeData["latitude"] as Double,
-                            challengeData["longitude"] as Double,
-                            results
-                        )
 
-                        if (results[0] <= 50) {
-                            completeChallenge(challengeData)
-                        }
-                    }
+                val resultsDistance = FloatArray(1)
+                Location.distanceBetween(
+                        previousLocation.latitude,
+                        previousLocation.longitude,
+                        lastLocation.latitude,
+                        lastLocation.longitude,
+                        resultsDistance
+                )
+
+                distanceTraveled += resultsDistance[0]
+                previousLocation = lastLocation
+
+                val results = FloatArray(1)
+                Location.distanceBetween(
+                        lastLocation.latitude,
+                        lastLocation.longitude,
+                        challengeData["latitude"] as Double,
+                        challengeData["longitude"] as Double,
+                        results
+                )
+
+                if (results[0] <= 1) {
+                    completeChallenge(challengeData)
+                    break
                 }
             }
         }
@@ -240,9 +252,9 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         val challengeId: Int = (challengeData["challenge_id"] as Double).toInt()
 
         val bodyJson = Gson().toJson(hashMapOf(
-            "user_id" to userData["user_id"],
-            "pw" to userData["password"],
-            "challenge_id" to challengeId
+                "user_id" to userData["user_id"],
+                "pw" to userData["password"],
+                "challenge_id" to challengeId
         ))
         CoroutineScope(Dispatchers.IO).launch {
             val (_, response, _) = Fuel.post("${getString(R.string.host)}/api/complete_challenge")
@@ -254,12 +266,9 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 runOnUiThread {
                     val status = response.statusCode
                     if (status == 200) {
-                        val image = ImageView(ctx)
-                        image.setImageResource(R.drawable.opened_treasure_chest)
-
                         AlertDialog.Builder(ctx)
-                            .setTitle("Congratulations on completing the challenge, $challengeName! You get a point!")
-                            .setView(image)
+                            .setTitle("Congratulations!")
+                            .setMessage("Congratulations on completing the challenge, $challengeName! You get a point!")
                             .setPositiveButton("OK") { _, _ ->
                                 val intent = Intent(ctx, PickChallengeActivity::class.java).apply {
                                     putExtra("userData", userData)
