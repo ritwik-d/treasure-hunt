a8a8bb6f47d770178cb764e130722d3a5b73fe75
ritwik-d <ritwik.deshpande.8@gmail.com>
2021-05-24 16:56:19 -0700

    fixed bugs

--- backend/python/gunicorn_run.sh
+++ backend/python/gunicorn_run.sh
@@ -3,7 +3,7 @@
 function main {
   host='127.0.0.1:8000'
   echo "host: ${host}"
-  nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b $host main:app &
+  nohup gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b $host main:app --error-logfile /home/ritwik/logs/guni-err.log &
 }
 
 main
